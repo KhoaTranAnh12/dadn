2cd96d9ec2ccded253874c4e4ca4b318b7b2662f
KhoaTranAnh12 <131005608+KhoaTranAnh12@users.noreply.github.com>
2024-11-30 17:05:27 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,13 +7,13 @@ from auth.auth import checkUsingAuth
 import firebase_admin
 from firebase_admin import db,credentials
 from firebase_admin import firestore
-cred = credentials.Certificate("dadn-241-firebase-adminsdk-6ql6y-0b6819c9a9.json")
+cred = credentials.Certificate("cred.json")
 firebase_admin.initialize_app(cred, {"databaseURL":"https://dadn-241-default-rtdb.asia-southeast1.firebasedatabase.app/"})
-from flask_cors import CORS
+from flask_cors import CORS   
 app = Flask(__name__)
 CORS(app)
 from Adafruit_IO import Client,Feed
-aio = Client("Binhphan1447", "aio_HVMe06MWPZa389qNEosYAcixMjdR")
+aio = Client("Binhphan1447", "aio_xCZH123V0tfRUaQps3s3Y5t1x7ZU")
 import models.users as User
 @app.route('/users', methods=['GET'])
 def user_getUsers():#Done
@@ -80,25 +80,45 @@ def user_deleteUser():
         return {"response": res["status"], "status": 201}
 
 import models.sensors as Sensors
+# @app.route('/sensors', methods=['GET'])
+# def sensor_getSensors():
+#     feedname = request.args.get('feedname')
+#     auth = request.headers.get('Authorization')
+#     AuthenticatedUsername = checkUsingAuth(db,auth)
+#     if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
+#     if feedname == None:
+#         res = Sensors.Sensors.getAllSensors(db)
+#         if "error" in res.keys():
+#             return {"error": "No sensors found", "status": 404}
+#         return {"sensor": res["sensors"], "status": 200}
+#     else:
+#         res = Sensors.Sensors.getValue(db,feedname,aio)
+#         print(res)
+#         if "error" in res.keys():
+#             return {"error": "No sensors found", "status": 404}
+#         else:
+#             return {"value": res["value"], "status": 201}
 @app.route('/sensors', methods=['GET'])
 def sensor_getSensors():
-    feedname = request.args.get('feedname')
     auth = request.headers.get('Authorization')
     AuthenticatedUsername = checkUsingAuth(db,auth)
     if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
-    if feedname == None:
-        res = Sensors.Sensors.getAllSensors(db)
-        if "error" in res.keys():
-            return {"error": "No sensors found", "status": 404}
-        return {"sensor": res["sensors"], "status": 200}
+    res = Sensors.Sensors.getAllSensors(db)
+    if "error" in res.keys():
+        return {"error": "No sensors found", "status": 404}
+    return {"sensor": res["sensors"], "status": 200}
+@app.route('/sensors', methods=['POST'])
+def sensor_getSensorsWithFeedName():
+    feedname = request.get_json()['feedname']
+    auth = request.headers.get('Authorization')
+    AuthenticatedUsername = checkUsingAuth(db,auth)
+    if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
+    res = Sensors.Sensors.getValue(db,feedname,aio)
+    print(res)
+    if "error" in res.keys():
+        return {"error": "No sensors found", "status": 404}
     else:
-        res = Sensors.Sensors.getValue(db,feedname,aio)
-        print(res)
-        if "error" in res.keys():
-            return {"error": "No users found", "status": 404}
-        else:
-            return {"value": res["value"], "status": 201}
-
+        return {"value": res["value"], "status": 201}
 @app.route('/sensors/add', methods=['POST'])
 def sensor_addSensor():
     data = request.get_json()
@@ -141,6 +161,7 @@ def device_setValue():
     data = request.get_json()
     auth = request.headers.get('Authorization')
     AuthenticatedUsername = checkUsingAuth(db,auth)
+    print(AuthenticatedUsername)
     if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
     res = Devices.Devices.setValue(db,data["name"],data["value"],AuthenticatedUsername,aio)
     if "error" in res.keys():
@@ -150,23 +171,25 @@ def device_setValue():
 import models.devices as Devices
 @app.route('/devices', methods=['GET'])
 def device_getDevices():
-    feedname = request.args.get('feedname')
     auth = request.headers.get('Authorization')
     AuthenticatedUsername = checkUsingAuth(db,auth)
     if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
-    if feedname == None:
-        res = Devices.Devices.getAllDevices(db)
-        if "error" in res.keys():
-            return {"error": "No devices found", "status": 404}
-        return {"device": res["devices"], "status": 200}
+    res = Devices.Devices.getAllDevices(db)
+    if "error" in res.keys():
+        return {"error": "No devices found", "status": 404}
+    return {"device": res["devices"], "status": 200}
+@app.route('/devices', methods=['POST'])
+def device_getDevicesWithFeedName():
+    feedname = request.get_json()['feedname']
+    auth = request.headers.get('Authorization')
+    AuthenticatedUsername = checkUsingAuth(db,auth)
+    if not AuthenticatedUsername: return {"error": "Auth failed", "status": 401}
+    res = Devices.Devices.getValue(db,feedname,aio)
+    print(res)
+    if "error" in res.keys():
+        return {"error": "No devices found", "status": 404}
     else:
-        res = Devices.Devices.getValue(db,feedname,aio)
-        print(res)
-        if "error" in res.keys():
-            return {"error": "No users found", "status": 404}
-        else:
-            return {"value": res["value"], "status": 201}
-
+        return {"value": res["value"], "status": 201}
 @app.route('/devices/add', methods=['POST'])
 def device_addDevice():
     data = request.get_json()
